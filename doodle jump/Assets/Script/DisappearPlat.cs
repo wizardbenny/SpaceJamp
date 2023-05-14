708d643b2f391e999139d6f17dbb8f78aae9110d
Wizardbenny <wizardbenny2009@gmail.com>
2023-05-14 18:58:27 -0400

    disappear sorta fixed

--- doodle jump/Assets/Script/DisappearPlat.cs
+++ doodle jump/Assets/Script/DisappearPlat.cs
@@ -7,29 +7,47 @@ public class DisappearPlat : MonoBehaviour
     // Start is called before the first frame update
     private Color currentColor;
     private bool touchPlayer;
+    private 
     void Start()
     {
         currentColor = this.gameObject.GetComponent<SpriteRenderer>().color;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (touchPlayer)
-        {
-            currentColor.a -= 0.01f;
-            this.gameObject.GetComponent<SpriteRenderer>().color = currentColor;
-        }
-        if (currentColor.a <= 0)
-        {
-            Destroy(this.gameObject);
-        }
+        //if (touchPlayer)
+        //{
+        //    currentColor.a -= 0.01f;
+        //    this.gameObject.GetComponent<SpriteRenderer>().color = currentColor;
+        //}
+        //if (currentColor.a <= 0)
+        //{
+        //    Destroy(this.gameObject);
+        //}
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            touchPlayer = true;
+            bool isFall = collision.gameObject.GetComponent<Basketball>().IsFalling();
+            if (isFall)
+            {
+                StartCoroutine(Fading());
+            }
+            
+            
         }
+    }
+    IEnumerator Fading()
+    {
+        for (float i = currentColor.a; i>=0; i-=0.01f)
+        {
+            currentColor.a = i;
+            this.gameObject.GetComponent<SpriteRenderer>().color = currentColor;
+            yield return new WaitForEndOfFrame();
+        }
+        Destroy(this.gameObject);
     }
 }
